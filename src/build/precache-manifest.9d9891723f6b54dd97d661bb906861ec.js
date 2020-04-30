89f75c9a63f65b0ac24be8fd71230aab5400ce65
Joshua Li <ql197@scarletmail.rutgers.edu>
2020-04-29 22:35:52 -0400

    First Commit

---
+++ src/build/precache-manifest.9d9891723f6b54dd97d661bb906861ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0ecbd8b160724f9a7cc6debd93c9df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4159c2a1a6a4d3c40f2",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "0cf6d6148858952f572d",
+    "url": "/static/js/2.f2b8de2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f2b8de2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4159c2a1a6a4d3c40f2",
+    "url": "/static/js/main.085419bb.chunk.js"
+  },
+  {
+    "revision": "c6cd80a6fce542ddaee9",
+    "url": "/static/js/runtime-main.b9244b71.js"
+  }
+]);
